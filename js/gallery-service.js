3356ca8c61c9667643f85417fa94b8c44611bbd8
ShiranCohen86 <shiranc86@gmail.com>
2021-02-01 19:15:06 +0200

    render projects

---
+++ js/gallery-service.js
@@ -0,0 +1,33 @@
+var gProjs = [
+    {
+        id: "mineSweeper",
+        name: "mine sweeper",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem ",
+        url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"]
+    },
+    {
+        id: "sokoban",
+        name: "Sokoban",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem ",
+        url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"]
+    },
+    {
+        id: "sokoban",
+        name: "Sokoban",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem",
+        url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"]
+    }
+]
+
+function getProjects() {
+    return gProjs;
+}
